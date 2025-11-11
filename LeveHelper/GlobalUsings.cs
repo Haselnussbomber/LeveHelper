80f899c5ee612518fb0c09a2b0c9a2d3a2fe3a29
Haselnussbomber <mail@haselnussbomber.de>
2025-11-11 10:18:41 +0100

    Remove shared csproj, update HaselCommon

--- LeveHelper/GlobalUsings.cs
+++ LeveHelper/GlobalUsings.cs
@@ -1,4 +1,5 @@
 global using System;
 global using Dalamud.Bindings.ImGui;
+global using HaselCommon.Windows;
 global using Injectio.Attributes;
 global using static HaselCommon.Globals.ColorHelpers;
