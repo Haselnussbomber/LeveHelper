7a0e8a0a17ac24e897a8d9ff6828b0da29c363cb
Haselnussbomber <mail@haselnussbomber.de>
2026-06-22 14:59:23 +0200

    Switch to IAsyncDalamudPlugin

--- LeveHelper/Plugin.cs
+++ LeveHelper/Plugin.cs
@@ -1,4 +1,7 @@
+using System.Threading;
+using System.Threading.Tasks;
 using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
 using HaselCommon.Extensions;
 using LeveHelper.Config;
 using Microsoft.Extensions.DependencyInjection;
@@ -6,29 +9,32 @@ using Microsoft.Extensions.Hosting;
 
 namespace LeveHelper;
 
-public sealed class Plugin : IDalamudPlugin
+[AutoConstruct]
+public partial class Plugin : IAsyncDalamudPlugin
 {
-    private readonly IHost _host;
+    private readonly IDalamudPluginInterface _pluginInterface;
+    private readonly IPluginLog _pluginLog;
+    private readonly IFramework _framework;
+    private IHost? _host;
 
-    public Plugin(IDalamudPluginInterface pluginInterface)
+    public Task LoadAsync(CancellationToken cancellationToken)
     {
         _host = new HostBuilder()
-            .UseContentRoot(pluginInterface.AssemblyLocation.Directory!.FullName)
+            .UseContentRoot(_pluginInterface.AssemblyLocation.Directory!.FullName)
             .ConfigureServices(services =>
             {
-                services.AddDalamud(pluginInterface);
-                services.AddSingleton(PluginConfig.Load);
+                services.AddDalamud(_pluginInterface);
+                services.AddConfig(PluginConfig.Load(_pluginInterface, _pluginLog));
                 services.AddHaselCommon();
                 services.AddLeveHelper();
             })
             .Build();
 
-        _host.Start();
+        return _host.StartOnFrameworkThread(_framework, cancellationToken);
     }
 
-    void IDisposable.Dispose()
+    public ValueTask DisposeAsync()
     {
-        _host.StopAsync().GetAwaiter().GetResult();
-        _host.Dispose();
+        return _host?.StopOnFrameworkThread(_framework) ?? ValueTask.CompletedTask;
     }
 }
